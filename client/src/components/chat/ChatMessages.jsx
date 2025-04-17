23c9faa2892f31f4c24a399c63c5d1604f5caa0d
Abhishek Kashyap <abhik5567@gmail.com>
2025-04-17 13:24:42 +0530

    add codeblock

--- client/src/components/chat/ChatMessages.jsx
+++ client/src/components/chat/ChatMessages.jsx
@@ -1,24 +1,58 @@
 import ReactMarkdown from "react-markdown";
+import { CodeBlock, atomOneDark } from "react-code-blocks";
+
 const components_style = {
     ul: ({ children }) => (
         <ul className="list-none pl-8 mb-4 text-gray-200">{children}</ul>
     ),
     ol: ({ children }) => (
-        <ol className="list-decimal pl-8 mb-4 text-gray-200">{children}</ol>
+        <ol className="list-decimal pl-6 mb-4 text-gray-200">{children}</ol>
     ),
     li: ({ children }) => (
         <li className="mb-2 pl-2 before:content-['•'] before:mr-2 before:text-blue-500">
             {children}
         </li>
     ),
+    strong: ({ children }) => (
+        <strong className="text-violet-400 font-bold">{children}</strong>
+    ),
+    p: ({ children }) => <p className="mb-4 text-gray-200">{children}</p>,
+    code: ({ node, inline, className, children, ...props }) => {
+        const match = /language-(\w+)/.exec(className || "");
+        if (inline) {
+            return (
+                <code
+                    className="bg-gray-700 text-gray-200 rounded px-1"
+                    {...props}
+                >
+                    {children}
+                </code>
+            );
+        } else if (match) {
+            return (
+                <CodeBlock
+                    text={String(children).replace(/\n$/, "")}
+                    language={match[1]}
+                    showLineNumbers={false}
+                    theme={atomOneDark}
+                    wrapLines={true}
+                />
+            );
+        }
+        return (
+            <code className="bg-gray-700 text-gray-200 rounded px-1" {...props}>
+                {children}
+            </code>
+        );
+    },
 };
 export default function ChatMessages({ messages }) {
     return (
-        <div className="h-96 overflow-y-auto border rounded p-4 bg-gray-5 bg-purple-300">
+        <div className="h- overflow-y-scroll border rounded p-4 bg-gray-5 bg-purple-300">
             <div className="mb-2 flex flex-col">
                 {messages.map((message, index) => (
                     <div key={index} className="flex flex-col">
-                        <div className="px-4 py-2 mb-2 rounded-lg self-end bg-blue-500 w-[300px] text-white">
+                        <div className="px-4 py-2 mb-2 rounded-lg self-end bg-blue-500 w-[400px] text-white">
                             {message["user_content"]}
                         </div>
                         {message["ai_content"] && (
