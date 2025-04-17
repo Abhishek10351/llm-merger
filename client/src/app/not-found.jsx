37a39daf5ebfb7689b917e91078f61c04c2358b0
Abhishek Kashyap <abhik5567@gmail.com>
2025-04-18 01:13:33 +0530

    add not found page

---
+++ client/src/app/not-found.jsx
@@ -0,0 +1,21 @@
+export default function NotFound() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold text-gray-800">
+                    404 - Not Found
+                </h1>
+                <p className="mt-4 text-lg text-gray-600">
+                    The page you are looking for does not exist.
+                </p>
+                <p className="mt-2 text-lg text-gray-600">
+                    Please check the URL or return to the{" "}
+                    <a href="/" className="text-blue-500 cursor-pointer">
+                        Home Page
+                    </a>
+                    .
+                </p>
+            </div>
+        </div>
+    );
+}
