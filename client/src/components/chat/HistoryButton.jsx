5bb08186ef1a29365f46feff783ffc7f7a96baab
Abhishek Kashyap <abhik5567@gmail.com>
2025-04-22 19:48:51 +0530

    change history button

--- client/src/components/chat/HistoryButton.jsx
+++ client/src/components/chat/HistoryButton.jsx
@@ -1,11 +1,11 @@
 import { useRouter } from "next/navigation";
-
-export default function HistoryButton({ id, title, onClick }) {
+import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export default function HistoryButton({ id, title }) {
     const router = useRouter();
 
     const handleClick = (e) => {
         e.stopPropagation();
-        onClick(id, title);
         router.push(`/chat/${id}`);
     };
 
@@ -18,23 +18,10 @@ export default function HistoryButton({ id, title, onClick }) {
             <div className="flex items-center justify-between">
                 <span className="text-sm font-semibold">{title}</span>
                 <button
-                    className="ml-2 text-red-500 hover:text-red-700 cursor-pointer"
-                    onClick={ handleClick}
+                    className="ml-2 text-grey-700 cursor-pointer hover:text-gray-900 focus:outline-none border-1 rounded-full p-1 transition duration-200 ease-in-out hover:bg-gray-300"
+                    // onClick={handleClick}
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-4 w-4"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M10 9a1 1 0 100-2 1 1 0 000 2zm0 4a1 1 0 100-2 1 1 0 000 2z"
-                            clipRule="evenodd"
-                        />
-                        <path d="M10.293 3.293a1 1 0 011.414 0l5.5 5.5a1 1 0 010 1.414l-5.5 5.5a1 1 0 01-1.414-1.414L14.086 10H6a1 1 0 110-2h8.086L10.293 4.707a1 1 0 010-1.414z" />
-                    </svg>
+                    <FontAwesomeIcon icon={faEllipsisV} size="lg" />
                 </button>
             </div>
         </div>
