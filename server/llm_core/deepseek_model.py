387f153c02917e81fe793cca40d62d2f6dc38754
Abhishek Kashyap <abhik5567@gmail.com>
2025-04-27 22:56:07 +0530

    add deepseek model

---
+++ server/llm_core/deepseek_model.py
@@ -0,0 +1,18 @@
+import os
+
+import dotenv
+
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import AIMessage, HumanMessage
+
+dotenv.load_dotenv()
+
+
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+
+if DEEPSEEK_API_KEY is None:
+    raise Exception("DEEPSEEK_API_KEY not found in environment variables")
+else:
+    deepseek_model = init_chat_model(
+        "deepseek-chat", model_provider="deepseek", api_key=DEEPSEEK_API_KEY
+    )
